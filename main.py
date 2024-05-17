26c0398a59cc655739708a702c3c49e52ef798a8
Luca <luca.carroz@epfl.ch>
2024-05-17 15:22:40 +0200

    added main.py to train on scitas

---
+++ main.py
@@ -0,0 +1,60 @@
+# from datasets import load_dataset
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from torch.utils.data.dataset import random_split
+# import jsonlines
+from helpers import format_data
+from helpers import train_on_paradetox
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # BART MODEL (STUDENT)
+    model_name = "facebook/bart-large"
+    student_tokenizer = AutoTokenizer.from_pretrained(model_name)
+    student_tokenizer.pad_token_id = student_tokenizer.eos_token_id
+    student_model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+    # POLITENESS JUDGE
+
+    # load tokenizer and model weights
+    classifier_tokenizer = RobertaTokenizer.from_pretrained('SkolkovoInstitute/roberta_toxicity_classifier')
+    toxicity_clasifier_model = RobertaForSequenceClassification.from_pretrained(
+        'SkolkovoInstitute/roberta_toxicity_classifier')
+
+    data_path = 'data/'
+    dataset = pd.read_csv(data_path + 'paradetox.tsv', sep='\t')
+
+    # Define the maximum length and other parameters
+    max_length = 38
+    tensor_dataset = format_data(max_length, student_tokenizer, dataset)
+
+    # Define the size of the training set
+    train_size = int(0.8 * len(tensor_dataset))
+    test_size = len(tensor_dataset) - train_size
+
+    # Split the dataset into training and test sets
+    train_dataset, test_dataset = random_split(tensor_dataset, [train_size, test_size])
+
+    # Defining parameters for training
+    batch_size = 16
+    lr = 3e-5
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+    optimizer = torch.optim.Adam(student_model.parameters(), lr=lr)
+    num_epochs = 1
+
+    train_on_paradetox(student_model=student_model,
+                       judge_model=toxicity_clasifier_model,
+                       tokenizer=student_tokenizer,
+                       judge_tokenizer=classifier_tokenizer,
+                       optimizer=optimizer,
+                       dataloader=train_dataloader,
+                       test_dataloader=test_dataloader,
+                       num_epochs=num_epochs,
+                       alpha=0.5,
+                       device=device,
+                       lambda_tox=1,
+                       lambda_bert=1)
